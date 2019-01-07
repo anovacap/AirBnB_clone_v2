baf3909a95f76b9646cef4e58199cc02e1677f40
robertglatzel <robertglatzel@gmail.com>
2019-01-07 21:34:22 +0000

    Progress on updating the models

--- models/review.py
+++ models/review.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 class Review(BaseModel):
     """This is the class for Review
@@ -10,6 +11,9 @@ class Review(BaseModel):
         user_id: user id
         text: review description
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), nullable=False, ForeignKey(places.id))
+    user_id = Column(String(60), nullable=False, ForeignKey(users.id))
+    # Need to update relationships. come back to number 9.
--- models/user.py
+++ models/user.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class User(BaseModel):
-    """This is the class for user
+class User(BaseModel, Base):
+    """This is the class for user.
+    Depending on the value of HBNB_TYPE_STORAGE, the new user will
+    be saved into either a mysql database, or into a file storage system.
+
     Attributes:
         email: email address
         password: password for you login
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    # Return to number 7 - must update relationships
+    #
+    #
+    #
