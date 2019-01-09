45e7c408be62d7f3a86bb310de19b227972f7ca9
anovacap <anovacap@yahoo.com>
2019-01-09 20:04:43 +0000

    City getter added

--- models/state.py
+++ models/state.py
@@ -5,6 +5,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -19,3 +21,12 @@ class State(BaseModel, Base):
                               backref='state')
     else:
         name = ""
+
+    @property
+    def cities(self):
+        """func cities getter"""
+        citi_list = []
+        for citi in models.storage.all(City).values():
+            if citi.state_id == self.id:
+                citi_list.append(citi)
+        return citi_list
