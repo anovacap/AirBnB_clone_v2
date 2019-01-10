9a1e3383ab6a562e1b5287adf512cdec7be5d72a
anovacap <anovacap@yahoo.com>
2019-01-10 03:33:18 +0000

    swap bacref cascade

--- models/place.py
+++ models/place.py
@@ -43,8 +43,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref='place',
-                               cascade="all, delete, delete-orphan")
+        reviews = relationship('Review', cascade="all, delete, delete-orphan",
+                               backref='place')
 
         amenities = relationship('Amenity', secondary=place_amenity,
                                  viewonly=False, backref="place_amenity")
