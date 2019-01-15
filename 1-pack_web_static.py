358266cdff8e072a5ab746c384d6af9771d75fe3
anovacap <anovacap@yahoo.com>
2019-01-15 03:23:13 +0000

    add ex.1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from the
+ contents of the web_static"""
+from fabric.api import local
+from time import strftime
+import os
+
+
+def do_pack():
+    """func - no args"""
+    now_time = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p version")
+        file = "version/web_static_{}.tgz".format(now_time)
+        local("tar -cvzf {} web_static/".format(file))
+        return file
+    except:
+        return None
