516dcb410719de435287f8d20e99851aaef4e319
anovacap <anovacap@yahoo.com>
2019-01-30 01:10:24 +0000

    add ex.7,8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def all_states():
+    return render_template('7-states_list.html', storage=storage.all('State'))
+
+
+@app.teardown_appcontext
+def teardown(error):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
